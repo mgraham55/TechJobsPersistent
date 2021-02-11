7d9a02063b52ef58cb52f871329698842135f2b6
Michael Graham <graphicmike55@gmail.com>
2021-02-11 16:27:14 -0600

    SQL commit

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,15 @@
 --Part 1
+Id INT(64)
+Name VARCHAR(255)
+EmployerId VARCHAR(255)
 
 --Part 2
+SELECT Name
+FROM employers
+WHERE LOCATION = "St. Louis City"
 
 --Part 3
-
+SELECT * FROM Skills
+LEFT JOIN JobSkills ON Skills.Id = JobSkills.SkillId
+WHERE JobSkills.JobId IS NOT NULL
+ORDER BY Name ASC;
